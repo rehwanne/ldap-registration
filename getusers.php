41657efd0dbb3f1099f774874447aa405f3b21f0
rehwanne <rehwanne@milliways.info>
2014-07-27 21:25:59 +0200

    Signed-off-by: rehwanne <rehwanne@milliways.info>

--- getusers.php
+++ getusers.php
@@ -1,9 +1,11 @@
 <?php
 require "init.php";
 
+$result = ldap_search($ldapconn,$ldap['base_dn'], "(cn=*)") or die ("Error in search query: ".ldap_error($ldapconn));
+$data = ldap_get_entries($ldapconn, $result);
 
-$sr=ldap_search($ldapconn,$ldap['base_dn'],'(uid='.$username.')');
-$info = ldap_get_entries($ldapconn, $sr);
-echo $info;
+foreach($data as $user){
+	echo "User: ". $user["mail"][0]."\n";
+}
 
 ?>
