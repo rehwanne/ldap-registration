e4de1f2dc3e775e58b37df3ac854dac5698ed3fb
Reh Wanne <rehwanne@milliways.info>
2014-07-29 16:11:21 +0000

    Fixed more stuff

--- delete.php
+++ delete.php
@@ -3,15 +3,16 @@
 require "init.php";
 
 $username=$argv[1];
-$db = $argv[2];
-  
-if(isset($db)){
-  if($db == "sql")
+if(isset($arv[2])){
+	$backend=$argv[2];
+  }
+if(isset($backend)){
+  if($backend == "sql")
     {
       $stmt=$db->prepare('DELETE FROM regs WHERE username = ?');
       $stmt->bind_param('s',$username);
       $stmt->execute();
-  }elseif("$db == ldap"){
+  }elseif("$backend == ldap"){
       if(ldap_delete($ldapconnm,'dn='.$username.','.$ldap['dc'])){
         echo "Successfully deleted";
         }else{
