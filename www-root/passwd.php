e52d12afc01529b646c3fd301b5973685e4c89d4
TuxCoder <git@o-g.at>
2014-08-30 20:30:37 +0200

    [!] bugfixes in passwd.php

--- www-root/passwd.php
+++ www-root/passwd.php
@@ -12,33 +12,34 @@ $password_new2=$_POST['password_new2'];
 $errors=array();
 
 if($password_new!==$password_new2) {
-        $errors[]="not smame password";
+  $errors[]="not smame password";
 }
 
 if(strlen($password_new)<6) {
-        $errors[]="password must be minimum 6 chars long";
+  $errors[]="password must be minimum 6 chars long";
 }
 if(!preg_match('/^[a-zA-Z0-9_-]+$/',$username)===1 && strlen($username)>=2) {
-        $errors[]="username only [a-zA-Z0-9_-] and minimum lenght of 2";
+  $errors[]="username only [a-zA-Z0-9_-] and minimum lenght of 2";
 }
 
 
 if(sizeof($errors)==0) {
-        $ldapconn_passwd = ldap_connect($ldap['host']);
-        $ldapbind_passwd = @ldap_bind($ldapconni_passwd,"uid=$username,ou=users,dc=milliways,dc=info",$password);
-        if($ldapbind_passwd) {
-                $errors[]="password is wrong";
-        }
+  $ldapconn_passwd = ldap_connect($ldap['host']);
+  ldap_set_option($ldapconn_passwd, LDAP_OPT_PROTOCOL_VERSION, 3);
+  $ldapbind_passwd = @ldap_bind($ldapconni_passwd,"uid=$username,ou=users,dc=milliways,dc=info",$password_old);
+  if(!$ldapbind_passwd) {
+    $errors[]="password is wrong";
+  }
 }
 
 if(sizeof($errors)!==0) {
-        echo '<p>'.implode('</p><p>',$errors).'</p>';
-        die();
+  echo '<p>'.implode('</p><p>',$errors).'</p>';
+  die();
 }
 
 
-ldap_mod_replace()
-$info["userPassword"] = "{crypt}" . crypt($password,'$6$'.generateSalt(10).'$');
+$info=array();
+$info["userPassword"] = "{crypt}" . crypt($password_new,'$6$'.generateSalt(10).'$');
 
 $result = ldap_mod_replace($ldapconn, "uid=$username,ou=users,dc=milliways,dc=info", $info);
 
